9841937f7eefd239e6580082bcd54a95bd85c30c
Luke <yjsmk0902@gmail.com>
2022-09-26 16:29:40 +0900

    프론트 수정

--- src/main/java/example/show/web/controller/SearchController.java
+++ src/main/java/example/show/web/controller/SearchController.java
@@ -2,6 +2,8 @@ package example.show.web.controller;
 
 import example.show.domain.entity.SearchForm;
 import example.show.domain.entity.SearchType;
+import example.show.domain.entity.Show;
+import example.show.domain.repository.ShowRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -12,11 +14,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/search")
 public class SearchController {
+
+    private static final ShowRepository showRepository = new ShowRepository();
 
     @ModelAttribute("searchTypes")
     public SearchType[] searchType() {
@@ -25,7 +31,9 @@ public class SearchController {
 
     @GetMapping
     public String searchForm(Model model) {
+        List<Show> shows = showRepository.findAll();
         model.addAttribute("searchForm", new SearchForm());
+        model.addAttribute("shows", shows);
         return "shows";
     }
 
